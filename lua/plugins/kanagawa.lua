ff9a51c1b8cfba88a1e10e91f755aa8b66786b03
Takuya-Mukai <takuya.mukai@gmail.com>
2024-02-02 14:23:04 +0900

    for thai

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,66 +1,76 @@
-
--- Get the colors for the current theme
+-- Default options:
 require('kanagawa').setup({
-  compile = true,
-  theme = "wave",
-  background = {
-  dark = "wave"
-  },
-  overrides = function(colors)
-    local theme = colors.theme
-    return {
-      NormalFloat = { bg = "none" },
-      FloatBorder = { bg = "none" },
-      FloatTitle = { bg = "none" },
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+      palette = {},
+      theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+      local theme = colors.theme
+      return {
+        TelescopeTitle = { fg = theme.ui.special, bold = true },
+        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
 
-      -- Save an hlgroup with dark background and dimmed foreground
-      -- so that you can use it where your still want darker windows.
-      -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-      NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
+        FloatTitle = { bg = "none" },
 
-      -- Popular plugins that open floats will link to NormalFloat by default;
-      -- set their background accordingly if you wish to keep them dark and borderless
-      LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-      MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-      TelescopeTitle = { fg = theme.ui.special, bold = true },
-      TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-      TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-      TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-      TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-      TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-      TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+        -- Save an hlgroup with dark background and dimmed foreground
+        -- so that you can use it where your still want darker windows.
+        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
-      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-      PmenuSbar = { bg = theme.ui.bg_m1 },
-      PmenuThumb = { bg = theme.ui.bg_p2 },
-    }
-end,
+        -- Popular plugins that open floats will link to NormalFloat by default;
+        -- set their background accordingly if you wish to keep them dark and borderless
+        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
+        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+        PmenuSbar = { bg = theme.ui.bg_m1 },
+        PmenuThumb = { bg = theme.ui.bg_p2 },
+      }
+    end,
+    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+      dark = "wave",           -- try "dragon" !
+      light = "lotus"
+    },
 })
 
+-- setup must be called before loading
 vim.cmd("colorscheme kanagawa")
---local highlight = {
---    "RainbowRed",
---    "RainbowYellow",
---    "RainbowBlue",
---    "RainbowOrange",
---    "RainbowGreen",
---    "RainbowViolet",
---    "RainbowCyan",
---}
+-- Get the colors for the current theme
+local colors = require("kanagawa.colors").setup()
+local palette_colors = colors.palette
+local theme_colors = colors.theme
 
---local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
---hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
---    vim.api.nvim_set_hl(0, "RainbowRed", { fg = palette.autumnRed })
---    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = palette.dragonYellow })
---    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = palette.dragonBlue2 })
---    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = palette.dragonOrange })
---    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = palette.dragonGreen2 })
---    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = palette.dragonPink })
---    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = palette.dragonAqua })
---end)
---
---require("ibl").setup { indent = { highlight = highlight } }
+-- Get the colors for a specific theme
+local wave_colors = require("kanagawa.colors").setup({ theme = 'wave' })
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "kanagawa",
+    callback = function()
+        if vim.o.background == "light" then
+            vim.fn.system("kitty +kitten themes Kanagawa_light")
+        elseif vim.o.background == "dark" then
+            vim.fn.system("kitty +kitten themes Kanagawa_dragon")
+        else
+            vim.fn.system("kitty +kitten themes Kanagawa")
+        end
+    end,
+})
